61057650299845a79724834bde497a4207ea6f1e
Jun Wei Tay <jwpro637@gmail.com>
2026-06-22 18:02:18 +0800

    Add: is-palindrome - submission-16

---
+++ Data Structures & Algorithms/is-palindrome/submission-16.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        alnum = ""
+        for char in s:
+            if char.isalnum():
+                alnum += char
+
+        if alnum.lower() != alnum.lower()[::-1]:
+            return False
+        return True
