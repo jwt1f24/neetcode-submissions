459103190712fd7cb8ab9cc45c60ea8c780b8aea
Jun Wei Tay <jwpro637@gmail.com>
2026-06-22 14:18:56 +0800

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False # anagrams must have same word length
+        
+        d1 = dict()
+        for char in s:
+            if char in d1:
+                d1[char] += 1
+            else:
+                d1[char] = 1
+
+        d2 = dict()
+        for char in t:
+            if char in d2:
+                d2[char] += 1
+            else:
+                d2[char] = 1
+        
+        for key in d1:
+            if key not in d2 or d1[key] != d2[key]: # check if key exists w/o accessing dict
+                return False
+
+        return True
+            
