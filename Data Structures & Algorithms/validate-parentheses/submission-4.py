e7edbbd3aa349df3a850c07dc19f5b0931002145
Jun Wei Tay <jwpro637@gmail.com>
2026-06-23 13:50:01 +0800

    Add: validate-parentheses - submission-4

---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        pr = {')':'(', ']':'[', '}':'{'}
+        stack = []
+
+        for c in s:
+            if c in pr:
+                # if stack is not empty & the last char is a closing bracket
+                if stack and stack[-1] == pr[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+        
+        # True if stack is empty
+        return True if not stack else False
+        
