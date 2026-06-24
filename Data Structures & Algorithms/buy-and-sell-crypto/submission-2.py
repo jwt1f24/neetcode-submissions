ef6f97f8443a1411e33cc36de219d67824afb84a
Jun Wei Tay <jwpro637@gmail.com>
2026-06-24 18:06:59 +0800

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        buy = prices[0]
+        maxpft = 0
+
+        for i in prices:
+            # current profit
+            profit = i - buy
+            if maxpft < profit:
+                maxpft = profit
+            
+            # find min value for buy price
+            if buy >= i:
+                buy = i
+        return maxpft
+
+        # time comp. = O(n), day count grows based on input size
+        # space comp. = O(1), var 'buy' & 'maxpft' are constant
